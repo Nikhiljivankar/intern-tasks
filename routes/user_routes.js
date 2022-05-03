b0e92ee0dbe16761b6c874a9f313616a09c9861a
nikhil-autotech <nikhil.autotech@gmail.com>
2022-05-03 18:14:58 +0530

    removed basic files

--- routes/user_routes.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const userController = require("../controllers/user_controller");
-
-router.post("/", userController.createUserData);
-
-module.exports = router;
